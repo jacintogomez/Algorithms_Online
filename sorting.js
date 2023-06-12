ccd1714ed53b1353027226b23be8160ba9deaa01
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2023-06-12 19:03:28 -0400

    main and basic functions

--- sorting.js
+++ sorting.js
@@ -1,9 +1,38 @@
+//main code for display.html
+
 outputdiv=document.getElementById("output");
-function main(al,arr){
-    if(al==="merge"){
-        appendtext(arr);
+
+function main(choice,s){
+    var ar=makeintoarray(s);
+    switch(choice){
+        case 'merge':
+            print(ar);
+            merge(ar);
+        case 'insert':
+            insert(ar);
     }
 }
+
+function print(ar){
+    addtext('[');
+    for(let x=0;x<ar.length;x++){
+        addtext(ar[x].toString()+' ');
+    }
+    appendtext(']');
+}
+
+function makeintoarray(s){
+    var real=[];
+    for(let x=0;x<s.length;x+=2){
+        real.push(parseInt(s[x]));
+    }
+    return real;
+}
+
 function appendtext(text){
     outputdiv.innerHTML+=text+'<br>';
+}
+
+function addtext(text){
+    outputdiv.innerHTML+=text;
 }
