f309382a2e1b13a80a8237541ad93f4ce769e1d5
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2023-06-12 13:39:56 -0400

    first stuff

---
+++ console.js
@@ -0,0 +1,8 @@
+function handleInput() {
+      var inputElement = document.getElementById('input');
+      var command = inputElement.value;
+      var consoleElement = document.getElementById('console');
+      consoleElement.innerHTML += '> ' + command + '<br>';
+      inputElement.value = '';
+
+}
